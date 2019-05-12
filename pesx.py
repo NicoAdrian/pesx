afadbd923f34950408ab787ec5890808e51e96e6
NicoAdrian <nicolasadrian3@gmail.com>
2019-05-12 21:17:26 +0200

    add script

---
+++ pesx.py
@@ -0,0 +1,24 @@
+import struct
+import sys
+
+with open(sys.argv[1], "rb") as fd:
+    while True:
+        pkt = fd.read(188)
+        if not pkt:
+            break
+        try:
+            pkt_header = struct.unpack(">I", pkt[:4])[0]
+        except:
+            break
+        sync = pkt_header >> 24 == 0x47
+        afc = (pkt_header >> 4) & 0x3 == 0b10 or (pkt_header >> 4) & 0x3 == 0b11
+        payload = (pkt_header >> 4) & 0x3 == 0b01 or (pkt_header >> 4) & 0x3 == 0b11
+        dat = pkt[4:]
+        if not sync:
+            print("No sync byte !")
+        if not payload:
+            pass
+        elif not afc:
+            print(dat)
+        else:
+            print(dat[dat[0] + 1 :])
